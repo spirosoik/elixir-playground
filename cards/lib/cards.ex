b7da99cd64ef293a5115e8e00f61039fe6bdccca
Spiros Economakis <spiros.oikonomakis@gmail.com>
2020-11-17 17:01:52 +0200

    Latest updates for loading files and create hand

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -16,6 +16,17 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -23,5 +34,16 @@ defmodule Cards do
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "The file doesn't exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck |> Cards.shuffle |> Cards.deal(hand_size)
   end
 end
